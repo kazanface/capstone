f01736ec94fae4402cb85f6c0095a2b59dd869a8
NSCCStudent <w0431256@nscc.ca>
2020-03-19 12:43:37 -0300

    Fixed contact btn that did not work on other pages

--- php/includes/navbar.php
+++ php/includes/navbar.php
@@ -38,6 +38,7 @@
 
 
 
+
     <div class="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">

4beafc876ab75ddec28d14b730f2da0a48a7df9f
LynnDarinKim <w0421043@nscc.ca>
2020-04-02 03:32:45 -0300

    clickable status search, number display

--- php/cust_pages/cust_orders.php
+++ php/cust_pages/cust_orders.php
@@ -3,16 +3,15 @@
     checkIfCustLoggedIn();
     include("../includes/dbConn.php");
     $conn = connectToDB();
+    include("../../")
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <?php include_once("../includes/head.php"); ?>
     <link rel="stylesheet" href="../../css/cust_admin.css">
-    <title>Customer Orders</title>
+    <title>View Order Status</title>
 </head>
 
 <body>
@@ -25,11 +24,22 @@
         </div>
         <hr>
         <div class="my-4">
-            <a href="cust_inquiries.php">All</a>  |
-            <a href="cust_new_inquiry.php">Pending payment</a>  |
-            <a href="cust_new_inquiry.php">Processing</a>  |
-            <a href="cust_new_inquiry.php">On hold</a>  |
-            <a href="cust_new_inquiry.php">Completed</a>
+            <?php
+                $numAll = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM Orders"));
+                $numPendingPayment = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM Orders WHERE ShipCity LIKE 'Pending payment'"));
+                $numProcessing = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM Orders WHERE ShipCity LIKE 'Processing'"));
+                $numOnHold = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM Orders WHERE ShipCity LIKE 'On hold'"));
+                $numCompleted = mysqli_num_rows(mysqli_query($conn,"SELECT * FROM Orders WHERE ShipCity LIKE 'Completed'"));
+            ?>
+            <form class="d-flex align-items-center " action="" method="post">
+                <div class="btn-status">
+                    <input class="p-1" type="submit" name="All" value="All">(<?php echo $numAll?>)<div class="vertical-line"></div>
+                    <input class="p-1" type="submit" name="PendingPayment" value="Pending payment">(<?php echo $numPendingPayment?>)<div class="vertical-line"></div>
+                    <input class="p-1" type="submit" name="Processing" value="Processing">(<?php echo $numProcessing?>)<div class="vertical-line"></div>
+                    <input class="p-1" type="submit" name="OnHold" value="On hold">(<?php echo $numOnHold?>)<div class="vertical-line"></div>
+                    <input class="p-1" type="submit" name="Completed" value="Completed">(<?php echo $numCompleted?>)<div class="vertical-line"></div>
+                </div>
+            </form>
         </div>
 
         <!-- TABLE -->
@@ -50,7 +60,19 @@
                 </thead>
                 <tbody>
                 <?php
-                    $result = mysqli_query($conn,"SELECT * FROM Orders");
+
+                $result = mysqli_query($conn,"SELECT * FROM Orders");
+
+                if(!empty($_POST['PendingPayment'])){
+                    $result = mysqli_query($conn,"SELECT * FROM Orders WHERE ShipCity LIKE 'Pending payment'");
+                } elseif(!empty($_POST['Processing'])){
+                    $result = mysqli_query($conn,"SELECT * FROM Orders WHERE ShipCity LIKE 'Processing'");
+                } elseif(!empty($_POST['OnHold'])){
+                    $result = mysqli_query($conn,"SELECT * FROM Orders WHERE ShipCity LIKE 'On hold'");
+                } elseif(!empty($_POST['Completed'])){
+                    $result = mysqli_query($conn,"SELECT * FROM Orders WHERE ShipCity LIKE 'Completed'");
+                }
+
                     while ($row = mysqli_fetch_assoc($result)):
                 ?>
 
